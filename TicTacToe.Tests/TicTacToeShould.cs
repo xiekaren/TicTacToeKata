b017b33c036aed7c95c076f2d48906b2169d7fe1
Karen Xie <karen-x@outlook.com>
2017-11-13 09:19:48 +1300

    add board and tests

---
+++ TicTacToe.Tests/TicTacToeShould.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using NUnit.Framework;
+
+namespace TicTacToe.Tests
+{
+    [TestFixture]
+    public class TicTacToeShould
+    {
+        [Test]
+        public void InitialiseEmpty3x3Grid()
+        {
+            var board = new Board(3, 3);
+            Assert.AreEqual("", board.Get(0, 0));
+            Assert.AreEqual("", board.Get(0, 1));
+            Assert.AreEqual("", board.Get(0, 2));
+            Assert.AreEqual("", board.Get(1, 0));
+            Assert.AreEqual("", board.Get(1, 1));
+            Assert.AreEqual("", board.Get(1, 2));
+            Assert.AreEqual("", board.Get(2, 0));
+            Assert.AreEqual("", board.Get(2, 1));
+            Assert.AreEqual("", board.Get(2, 2));
+        }
+
+        [Test]
+        public void SetValueAtPosition()
+        {
+            var board = new Board(3, 3);
+            board.Set(0, 0, "x");
+            Assert.AreEqual("x", board.Get(0, 0));
+        }
+    }
+
+
+}
---
+++ TicTacToe/Board.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+
+namespace TicTacToe
+{
+    public class Board
+    {
+        private readonly Hashtable _board;
+
+        public Board(int width, int height)
+        {
+            _board = new Hashtable();
+            Initialise(width, height);
+        }
+
+        private void Initialise(int width, int height)
+        {
+            for (var i = 0; i < height; i++)
+            {
+                for (var j = 0; j < width; j++)
+                {
+                    var key = $"{i}{j}";
+                    _board.Add(key, "");
+                }
+            }
+        }
+
+        public string Get(int x, int y)
+        {
+            var key = $"{x}{y}";
+
+            if (_board.ContainsKey(key))
+            {
+                return (string)_board[key];
+            }
+
+            throw new ArgumentException("Position is not on grid.");
+        }
+
+        public void Set(int x, int y, string value)
+        {
+            var key = $"{x}{y}";
+            _board[key] = value;
+        }
+    }
+}
