c1d83378ff5a4085ce69b6a32dc7a124013a2510
Karen Xie <karen-x@outlook.com>
2017-11-14 08:31:45 +1300

    remove unnecessary methods

--- TicTacToe.Tests/TicTacToeShould.cs
+++ TicTacToe.Tests/TicTacToeShould.cs
@@ -20,28 +20,5 @@ namespace TicTacToe.Tests
         {
             Assert.AreEqual(3, _ticTacToe.Solve("XOX   XO "));
         }
-
-        [Test]
-        [TestCase("XX ", "X")]
-        [TestCase("X X", "X")]
-        public void ReturnTrueWhenPlayerCanWin(string line, string playerToken)
-        {
-            Assert.IsTrue(_ticTacToe.CanWin(line, playerToken));
-        }
-
-        [Test]
-        [TestCase("X 0", "X")]
-        [TestCase("XX0", "X")]
-        public void ReturnFalseWhenPlayerCannotWin(string line, string playerToken)
-        {
-            Assert.IsFalse(_ticTacToe.CanWin(line, playerToken));
-        }
-
-        [Test]
-        public void GetRows()
-        {
-            CollectionAssert.AreEqual(new List<string> {"XXX", "OOO", "XXX"}, _ticTacToe.GetRows("XXXOOOXXX", 3, 3));
-        }
-
     }
 }
--- TicTacToe/TicTacToe.cs
+++ TicTacToe/TicTacToe.cs
@@ -10,28 +10,5 @@ namespace TicTacToe
         {
             return board.IndexOf(" ", StringComparison.Ordinal);
         }
-
-        public bool CanWin(string line, string playerToken)
-        {
-            return line.Count(x => x.ToString() == playerToken) == 2 && line.Contains(" ");
-        }
-
-        public List<string> GetRows(string board, int rowWidth, int numRows)
-        {
-            var rows = new List<string>();
-
-            for (var i = 0; i < numRows; i++)
-            {
-                var row = "";
-
-                for (var j = 0; j < rowWidth; j++)
-                {
-                    row += board[3*i + j];
-                }
-                rows.Add(row);
-            }
-
-            return rows;
-        }
     }
 }
