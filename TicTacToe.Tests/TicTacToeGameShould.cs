0ffe520345c29bc70cb654ca4a902816ac0afcf3
Karen Xie <karen-x@outlook.com>
2017-11-20 21:46:01 +1300

    starting from square one

---
+++ TicTacToe.Tests/TicTacToeGameShould.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+
+namespace TicTacToe.Tests
+{
+    [TestFixture]
+    public class TicTacToeGameShould
+    {
+        [Test]
+        public void EndWhenTheBoardIsFilled()
+        {
+            var renderer = new Renderer();
+            var board = new Board("OXX" +
+                                  "OXX" +
+                                  "XOO");
+            var game = new TicTacToeGame(renderer, board);
+            Assert.AreEqual("Helo", board);
+        }
+    }
+
+    public class Board
+    {
+        private string _board;
+
+        public Board(string input)
+        {
+            _board = input;
+        }
+    }
+
+    public class TicTacToeGame
+    {
+        private Renderer _renderer;
+
+        public TicTacToeGame(Renderer renderer, Board board)
+        {
+            _renderer = renderer;
+        }
+    }
+
+    public class Renderer
+    {
+    }
+}
