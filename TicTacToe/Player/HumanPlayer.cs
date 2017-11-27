2d5f96c4f0fe8db9937c74adf3566e2675c9ab72
Karen Xie <karen-x@outlook.com>
2017-11-28 08:06:30 +1300

    restructure project

---
+++ TicTacToe/Player/HumanPlayer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TicTacToe
+{
+    public class HumanPlayer  : IPlayer
+    {
+        public readonly string Token = "O";
+
+        public int Solve(Board board)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
