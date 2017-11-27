070a4a34cb61306f284275b47d85cc3c39fd0cd3
Karen Xie <karen-x@outlook.com>
2017-11-28 09:35:29 +1300

    solve tests for human player

--- TicTacToe.Tests/Player/HumanPlayerShould.cs
+++ TicTacToe.Tests/Player/HumanPlayerShould.cs
@@ -1,9 +1,23 @@
-﻿using NUnit.Framework;
+﻿using Moq;
+using NUnit.Framework;
+using TicTacToe.Checker;
+using TicTacToe.GameElements;
+using TicTacToe.Player;
 
 namespace TicTacToe.Tests.Player
 {
     [TestFixture]
     public class HumanPlayerShould
     {
+        public void SolveWithValidInput()
+        {
+            var mockConsole = new Mock<ConsoleWrapper>();
+            mockConsole.Setup(t => t.ReadLine()).Returns("1");
+            var player = new HumanPlayer(new InputChecker(), mockConsole.Object);
+
+            var input = player.Solve(new Board());
+
+            Assert.AreEqual(1, input);
+        }
     }
 }
--- TicTacToe/Player/HumanPlayer.cs
+++ TicTacToe/Player/HumanPlayer.cs
@@ -1,13 +1,49 @@
 ﻿using System;
+using TicTacToe.Checker;
 using TicTacToe.GameElements;
 
 namespace TicTacToe.Player
 {
     public class HumanPlayer : IPlayer
     {
+        private readonly InputChecker _inputChecker;
+        private readonly ConsoleWrapper _consoleWrapper;
+
+        public HumanPlayer(InputChecker inputChecker, ConsoleWrapper consoleWrapper)
+        {
+            _inputChecker = inputChecker;
+            _consoleWrapper = consoleWrapper;
+        }
+
         public int Solve(Board board)
         {
-            throw new NotImplementedException();
+            while (true)
+            {
+                try
+                {
+                    var input = _consoleWrapper.ReadLine();
+                    _inputChecker.Validate(board, input);
+                    return int.Parse(input);
+                }
+                catch (ArgumentException e)
+                {
+                    _consoleWrapper.WriteLine(e.Message);
+                }
+            }
+
+        }
+    }
+
+    public class ConsoleWrapper
+    {
+        public virtual string ReadLine()
+        {
+            return Console.ReadLine();
+        }
+
+        public virtual void WriteLine(string line)
+        {
+            Console.WriteLine(line);
         }
     }
 }
