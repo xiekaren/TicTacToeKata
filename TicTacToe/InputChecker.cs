c2088e0427fc3782c4754642e6f7a6637ec0f53c
Karen Xie <karen-x@outlook.com>
2017-11-28 08:00:51 +1300

    refactor input checker

--- TicTacToe/InputChecker.cs
+++ TicTacToe/InputChecker.cs
@@ -4,18 +4,18 @@ namespace TicTacToe
 {
     public class InputChecker
     {
+        private const string EnterAPositionOnTheBoard = "Please enter a position from 1 to 9.";
+        private const string EnterAnUnoccupiedPosition = "Please enter an unoccupied position from 1 to 9.";
+
         public void Validate(Board board, string input)
         {
             int inputAsNumber;
 
-            if (!int.TryParse(input, out inputAsNumber))
-                throw new ArgumentException("Please enter a position from 1 to 9.");
-
-            if (inputAsNumber < 0 || inputAsNumber > board.Length)
-                throw new ArgumentException("Please enter a position from 1 to 9.");
+            if (string.IsNullOrEmpty(input) || !int.TryParse(input, out inputAsNumber) || inputAsNumber < 0 || inputAsNumber > board.Length)
+                throw new ArgumentException(EnterAPositionOnTheBoard);
 
             if (board.GetTokenAt(inputAsNumber) != "-")
-                throw new ArgumentException("Please enter an unoccupied position from 1 to 9.");
+                throw new ArgumentException(EnterAnUnoccupiedPosition);
         }
     }
 }
