bea6ceeeaadab02e0bda6cdc4701ba616366fdf5
Karen Xie <karen-x@outlook.com>
2017-12-02 18:24:24 +1300

    building first test to display board

---
+++ TicTacToe.Tests/TicTacToeShould.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+
+namespace TicTacToe.Tests
+{
+    [TestFixture]
+    public class TicTacToeShould
+    {
+        [Test]
+        public void DisplayBoard()
+        {
+            var input = new Board(3, 3);
+            var expected = "[-][-][-]" +
+                           "[-][-][-]" +
+                           "[-][-][-]";
+        }
+    }
+
+    public class Board
+    {
+        public Board(int width, int height)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
